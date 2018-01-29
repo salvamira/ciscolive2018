5c7a829c8de874cce2363eeaebe198061917ae65
Salvador Mira <smira@iesperemaria.com>
2018-01-29 17:56:29 +0100

    Exercicis més capítol 2

---
+++ 08_parse-json5.py
@@ -0,0 +1,35 @@
+#Replace 'your_api_key' with your Google API key
+
+import urllib.parse
+import requests
+
+main_api = 'https://maps.googleapis.com/maps/api/geocode/json?'
+key = 'AIzaSyCrKeXet-8EdFMKnfz4DZzBHRsV1laCZ3Q'
+
+
+while True:
+    address = input('Address: ')
+    """if address == 'quit' or address == 'q':
+        break"""
+    
+    url = main_api + urllib.parse.urlencode({'address': address}) + '&key=' + key
+
+    print(url)
+
+    json_data = requests.get(url).json()
+
+    json_status = json_data['status']
+    print('API Status: ' + json_status + '\n')
+
+    if json_status == 'OK':
+        formatted_address = json_data['results'][0]['formatted_address']
+        print(formatted_address)
+
+
+    """if json_status == 'OK':
+        for each in json_data['results'][0]['address_components']:
+            print(each['long_name'])
+            
+        formatted_address = json_data['results'][0]['formatted_address']
+        print('\n' + formatted_address)
+"""
